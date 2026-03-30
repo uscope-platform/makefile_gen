d3bc945d78a254ecf042ce278a73784b2bd97571
Filippo Savi <filippo.savi@unimore.it>
2026-03-30 12:31:35 +0000

    added synth script test

--- Testing/Backend/project_generator.cpp
+++ Testing/Backend/project_generator.cpp
@@ -78,7 +78,7 @@ TEST(xilinx_project_gen, simple_gen){
     std::ostringstream result_tcl;
     generator.write_makefile(result_tcl);
 
-    std::string expected_tcl = "set project_name test_proj\nset origin_dir \".\"\nset base_dir /tmp/rb\nset commons_dir [list ]\nset synth_sources [list ]\nset sim_sources [list \"check_files/test_data/Components/Common/interfaces.sv\" \"check_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/AD2S1210.sv\" \"check_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/ad2s1210_CU.sv\" \"check_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/ad2s1210_configurator.sv\" \"check_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/ad2s1210_fault_handler.sv\" \"check_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/ad2s1210_reader.sv\" \"check_files/test_data/Components/ExternalDrivers/AD2S1210/tb/AD2S1210_tb.sv\" \"check_files/test_data/Components/ExternalDrivers/AD2S1210/tb/ad2s1210_tl_test.sv\" \"check_files/test_data/Components/comms/SPI/rtl/Spi.sv\" \"check_files/test_data/Components/comms/SPI/rtl/master/ClockGen.sv\" \"check_files/test_data/Components/comms/SPI/rtl/master/SpiControlUnit.sv\" \"check_files/test_data/Components/comms/SPI/rtl/master/SpiRegister.sv\" \"check_files/test_data/Components/comms/SPI/rtl/master/Spi_master.sv\" \"check_files/test_data/Components/comms/SPI/rtl/master/TransferEngine.sv\" \"check_files/test_data/Components/system/EnableGenerator/rtl/Enable_Generator_2.sv\" \"check_files/test_data/Components/system/EnableGenerator/rtl/enable_comparator.v\" \"check_files/test_data/Components/system/EnableGenerator/rtl/enable_generator_core.v\" \"check_files/test_data/Components/system/EnableGenerator/rtl/enable_generator_counter.v\" \"check_files/test_data/Components/system/axi_lite/crossbar/rtl/address_decoder.sv\" \"check_files/test_data/Components/system/axi_lite/crossbar/rtl/axil_crossbar.sv\" \"check_files/test_data/Components/system/axi_lite/crossbar/rtl/axil_crossbar_wrapper.sv\" \"check_files/test_data/Components/system/axi_lite/simple_register_cu/rtl/axil_simple_register_cu.sv\" \"check_files/test_data/Components/system/axi_lite/skid_buffer/rtl/axil_skid_buffer.sv\" \"check_files/test_data/Components/system/axi_stream/combiner/rtl/axi_stream_combiner_3.sv\" ]\nset constraints_sources [list ]\n# Create project\ncreate_project ${project_name} ./${project_name}\nset_property part xc7z020clg400-1 [current_project]\n# Set the directory path for the new project\nset proj_dir [get_property directory [current_project]]\nset obj [current_project]\nadd_files -norecurse $synth_sources\nset_property include_dirs $commons_dir [get_filesets sources_1]\nset_property SOURCE_SET sources_1 [get_filesets sim_1]\nadd_files -fileset sim_1 -norecurse $sim_sources\nset_property top AD2S1210_tb [get_filesets sim_1]\nupdate_compile_order\n";
+    const std::string expected_tcl = "set project_name test_proj\nset origin_dir \".\"\nset base_dir /tmp/rb\nset commons_dir [list ]\nset synth_sources [list ]\nset sim_sources [list \"check_files/test_data/Components/Common/interfaces.sv\" \"check_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/AD2S1210.sv\" \"check_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/ad2s1210_CU.sv\" \"check_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/ad2s1210_configurator.sv\" \"check_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/ad2s1210_fault_handler.sv\" \"check_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/ad2s1210_reader.sv\" \"check_files/test_data/Components/ExternalDrivers/AD2S1210/tb/AD2S1210_tb.sv\" \"check_files/test_data/Components/ExternalDrivers/AD2S1210/tb/ad2s1210_tl_test.sv\" \"check_files/test_data/Components/comms/SPI/rtl/Spi.sv\" \"check_files/test_data/Components/comms/SPI/rtl/master/ClockGen.sv\" \"check_files/test_data/Components/comms/SPI/rtl/master/SpiControlUnit.sv\" \"check_files/test_data/Components/comms/SPI/rtl/master/SpiRegister.sv\" \"check_files/test_data/Components/comms/SPI/rtl/master/Spi_master.sv\" \"check_files/test_data/Components/comms/SPI/rtl/master/TransferEngine.sv\" \"check_files/test_data/Components/system/EnableGenerator/rtl/Enable_Generator_2.sv\" \"check_files/test_data/Components/system/EnableGenerator/rtl/enable_comparator.v\" \"check_files/test_data/Components/system/EnableGenerator/rtl/enable_generator_core.v\" \"check_files/test_data/Components/system/EnableGenerator/rtl/enable_generator_counter.v\" \"check_files/test_data/Components/system/axi_lite/crossbar/rtl/address_decoder.sv\" \"check_files/test_data/Components/system/axi_lite/crossbar/rtl/axil_crossbar.sv\" \"check_files/test_data/Components/system/axi_lite/crossbar/rtl/axil_crossbar_wrapper.sv\" \"check_files/test_data/Components/system/axi_lite/simple_register_cu/rtl/axil_simple_register_cu.sv\" \"check_files/test_data/Components/system/axi_lite/skid_buffer/rtl/axil_skid_buffer.sv\" \"check_files/test_data/Components/system/axi_stream/combiner/rtl/axi_stream_combiner_3.sv\" ]\nset constraints_sources [list ]\n# Create project\ncreate_project ${project_name} ./${project_name}\nset_property part xc7z020clg400-1 [current_project]\n# Set the directory path for the new project\nset proj_dir [get_property directory [current_project]]\nset obj [current_project]\nadd_files -norecurse $synth_sources\nset_property include_dirs $commons_dir [get_filesets sources_1]\nset_property SOURCE_SET sources_1 [get_filesets sim_1]\nadd_files -fileset sim_1 -norecurse $sim_sources\nset_property top AD2S1210_tb [get_filesets sim_1]\nupdate_compile_order\n";
 
     EXPECT_EQ(expected_tcl, result_tcl.str());
 }
@@ -114,7 +114,7 @@ TEST(xilinx_project_gen, argumented_script_gen){
     gen.write_makefile(test_stream);
     auto result = test_stream.str();
 
-    std::string check = "set project_name proj_name\nset origin_dir \".\"\nset base_dir /test/dir\nset commons_dir [list \"/test/dir/include\" ]\nset synth_sources [list \"/test/synth/source.sv\" ]\nset sim_sources [list \"/test/sim/source.sv\" ]\nset constraints_sources [list \"/test/constr/source.sv\" ]\n# Create project\ncreate_project ${project_name} ./${project_name}\nset_property part xc7z020clg400-1 [current_project]\n# Set the directory path for the new project\nset proj_dir [get_property directory [current_project]]\nset obj [current_project]\nsource /test/script/source.sv\nscr_name 1\nscr_name 1\nadd_files -norecurse $synth_sources\nset_property top synth_tl [get_filesets sources_1]\nset_property include_dirs $commons_dir [get_filesets sources_1]\nset_property SOURCE_SET sources_1 [get_filesets sim_1]\nadd_files -fileset constrs_1 -norecurse  $constraints_sources\nadd_files -fileset sim_1 -norecurse $sim_sources\nset_property top sim_tl [get_filesets sim_1]\nupdate_compile_order\n";
+    const std::string check = "set project_name proj_name\nset origin_dir \".\"\nset base_dir /test/dir\nset commons_dir [list \"/test/dir/include\" ]\nset synth_sources [list \"/test/synth/source.sv\" ]\nset sim_sources [list \"/test/sim/source.sv\" ]\nset constraints_sources [list \"/test/constr/source.sv\" ]\n# Create project\ncreate_project ${project_name} ./${project_name}\nset_property part xc7z020clg400-1 [current_project]\n# Set the directory path for the new project\nset proj_dir [get_property directory [current_project]]\nset obj [current_project]\nsource /test/script/source.sv\nscr_name 1\nscr_name 1\nadd_files -norecurse $synth_sources\nset_property top synth_tl [get_filesets sources_1]\nset_property include_dirs $commons_dir [get_filesets sources_1]\nset_property SOURCE_SET sources_1 [get_filesets sim_1]\nadd_files -fileset constrs_1 -norecurse  $constraints_sources\nadd_files -fileset sim_1 -norecurse $sim_sources\nset_property top sim_tl [get_filesets sim_1]\nupdate_compile_order\n";
     ASSERT_EQ(result, check);
 }
 
@@ -180,10 +180,74 @@ TEST( xilinx_project_gen, sim_script) {
     std::ostringstream result_tcl;
     generator.write_sim_control_script(result_tcl);
 
-    std::string expected_sh = "FILES=( \n    /data/verilog/src/glbl.v\n    check_files/test_data/Components/Common/interfaces.sv\n    check_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/AD2S1210.sv\n    check_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/ad2s1210_CU.sv\n    check_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/ad2s1210_configurator.sv\n    check_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/ad2s1210_fault_handler.sv\n    check_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/ad2s1210_reader.sv\n    check_files/test_data/Components/ExternalDrivers/AD2S1210/tb/AD2S1210_tb.sv\n    check_files/test_data/Components/ExternalDrivers/AD2S1210/tb/ad2s1210_tl_test.sv\n    check_files/test_data/Components/comms/SPI/rtl/Spi.sv\n    check_files/test_data/Components/comms/SPI/rtl/master/ClockGen.sv\n    check_files/test_data/Components/comms/SPI/rtl/master/SpiControlUnit.sv\n    check_files/test_data/Components/comms/SPI/rtl/master/SpiRegister.sv\n    check_files/test_data/Components/comms/SPI/rtl/master/Spi_master.sv\n    check_files/test_data/Components/comms/SPI/rtl/master/TransferEngine.sv\n    check_files/test_data/Components/system/EnableGenerator/rtl/Enable_Generator_2.sv\n    check_files/test_data/Components/system/EnableGenerator/rtl/enable_comparator.v\n    check_files/test_data/Components/system/EnableGenerator/rtl/enable_generator_core.v\n    check_files/test_data/Components/system/EnableGenerator/rtl/enable_generator_counter.v\n    check_files/test_data/Components/system/axi_lite/crossbar/rtl/address_decoder.sv\n    check_files/test_data/Components/system/axi_lite/crossbar/rtl/axil_crossbar.sv\n    check_files/test_data/Components/system/axi_lite/crossbar/rtl/axil_crossbar_wrapper.sv\n    check_files/test_data/Components/system/axi_lite/simple_register_cu/rtl/axil_simple_register_cu.sv\n    check_files/test_data/Components/system/axi_lite/skid_buffer/rtl/axil_skid_buffer.sv\n    check_files/test_data/Components/system/axi_stream/combiner/rtl/axi_stream_combiner_3.sv\n)\n\nmkdir -p /tmp/tb/sim\ncp sim.tcl /tmp/tb/sim/sim.tcl\n\n\n(\n    cd /tmp/tb/sim|| exit\n\n    echo -e \"\\n\\033[1;33m>>> PHASE 1: XVLOG (Analysis) <<<\\033[0m\"\n    xvlog -sv \"${FILES[@]}\"  -i /data/rsb/busdef\n    if [ $? -ne 0 ]; then\n        echo -e \"\\033[1;31m!!! XVLOG FAILED !!!\\033[0m\"\n        exit 1\n    fi\n\n    echo -e \"\\n\\033[1;33m>>> PHASE 2: XELAB (Elaboration) <<<\\033[0m\"\n    xelab -debug typical --relax -top AD2S1210_tb -top glbl -L xil_defaultlib -L unisims_ver -L unimacro_ver -L xpm  -snapshot sim_snapshot  -timescale 10ns/1ps\n    if [ $? -ne 0 ]; then\n        echo -e \"\\033[1;31m!!! XELAB FAILED !!!\\033[0m\"\n        exit 1\n    fi\n\n    echo -e \"\\n\\033[1;33m>>> PHASE 3: XSIM (Simulation) <<<\\033[0m\"\n    xsim sim_snapshot -tclbatch sim.tcl\n    if [ $? -ne 0 ]; then\n        echo -e \"\\033[1;31m!!! XSIM FAILED !!!\\033[0m\"\n        exit 1\n    fi\n\n)\nif [ -f /tmp/tb/sim/dump.vcd  ]; then\n    vcd2fst /tmp/tb/sim/dump.vcd dump.fst\n    rm /tmp/tb/sim/dump.vcd\nfi\nrm -r /tmp/tb/sim\n";
-    std::string expected_tcl = "\nopen_vcd dump.vcd\n\nlog_vcd [get_objects -recursive /*]\n\nrun 2ms\n\nflush_vcd\nclose_vcd\nexit\n";
+    const std::string expected_sh = "FILES=( \n    /data/verilog/src/glbl.v\n    check_files/test_data/Components/Common/interfaces.sv\n    check_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/AD2S1210.sv\n    check_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/ad2s1210_CU.sv\n    check_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/ad2s1210_configurator.sv\n    check_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/ad2s1210_fault_handler.sv\n    check_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/ad2s1210_reader.sv\n    check_files/test_data/Components/ExternalDrivers/AD2S1210/tb/AD2S1210_tb.sv\n    check_files/test_data/Components/ExternalDrivers/AD2S1210/tb/ad2s1210_tl_test.sv\n    check_files/test_data/Components/comms/SPI/rtl/Spi.sv\n    check_files/test_data/Components/comms/SPI/rtl/master/ClockGen.sv\n    check_files/test_data/Components/comms/SPI/rtl/master/SpiControlUnit.sv\n    check_files/test_data/Components/comms/SPI/rtl/master/SpiRegister.sv\n    check_files/test_data/Components/comms/SPI/rtl/master/Spi_master.sv\n    check_files/test_data/Components/comms/SPI/rtl/master/TransferEngine.sv\n    check_files/test_data/Components/system/EnableGenerator/rtl/Enable_Generator_2.sv\n    check_files/test_data/Components/system/EnableGenerator/rtl/enable_comparator.v\n    check_files/test_data/Components/system/EnableGenerator/rtl/enable_generator_core.v\n    check_files/test_data/Components/system/EnableGenerator/rtl/enable_generator_counter.v\n    check_files/test_data/Components/system/axi_lite/crossbar/rtl/address_decoder.sv\n    check_files/test_data/Components/system/axi_lite/crossbar/rtl/axil_crossbar.sv\n    check_files/test_data/Components/system/axi_lite/crossbar/rtl/axil_crossbar_wrapper.sv\n    check_files/test_data/Components/system/axi_lite/simple_register_cu/rtl/axil_simple_register_cu.sv\n    check_files/test_data/Components/system/axi_lite/skid_buffer/rtl/axil_skid_buffer.sv\n    check_files/test_data/Components/system/axi_stream/combiner/rtl/axi_stream_combiner_3.sv\n)\n\nmkdir -p /tmp/tb/sim\ncp sim.tcl /tmp/tb/sim/sim.tcl\n\n\n(\n    cd /tmp/tb/sim|| exit\n\n    echo -e \"\\n\\033[1;33m>>> PHASE 1: XVLOG (Analysis) <<<\\033[0m\"\n    xvlog -sv \"${FILES[@]}\"  -i /data/rsb/busdef\n    if [ $? -ne 0 ]; then\n        echo -e \"\\033[1;31m!!! XVLOG FAILED !!!\\033[0m\"\n        exit 1\n    fi\n\n    echo -e \"\\n\\033[1;33m>>> PHASE 2: XELAB (Elaboration) <<<\\033[0m\"\n    xelab -debug typical --relax -top AD2S1210_tb -top glbl -L xil_defaultlib -L unisims_ver -L unimacro_ver -L xpm  -snapshot sim_snapshot  -timescale 10ns/1ps\n    if [ $? -ne 0 ]; then\n        echo -e \"\\033[1;31m!!! XELAB FAILED !!!\\033[0m\"\n        exit 1\n    fi\n\n    echo -e \"\\n\\033[1;33m>>> PHASE 3: XSIM (Simulation) <<<\\033[0m\"\n    xsim sim_snapshot -tclbatch sim.tcl\n    if [ $? -ne 0 ]; then\n        echo -e \"\\033[1;31m!!! XSIM FAILED !!!\\033[0m\"\n        exit 1\n    fi\n\n)\nif [ -f /tmp/tb/sim/dump.vcd  ]; then\n    vcd2fst /tmp/tb/sim/dump.vcd dump.fst\n    rm /tmp/tb/sim/dump.vcd\nfi\nrm -r /tmp/tb/sim\n";
+    const std::string expected_tcl = "\nopen_vcd dump.vcd\n\nlog_vcd [get_objects -recursive /*]\n\nrun 2ms\n\nflush_vcd\nclose_vcd\nexit\n";
 
-    auto dbg = result_tcl.str();
     EXPECT_EQ(expected_sh, result_sh.str());
+    EXPECT_EQ(expected_tcl, result_tcl.str());
+}
+
+
+
+TEST( xilinx_project_gen, synth_script) {
+
+
+    std::shared_ptr<data_store> d_store = std::make_shared<data_store>(true, "/tmp/test_data_store");
+    std::shared_ptr<settings_store> s_store = std::make_shared<settings_store>(true, "/tmp/test_data_store");
+
+    std::vector<std::string> paths = {
+            "Components/Common",
+            "Components/ExternalDrivers/AD2S1210/rtl",
+            "Components/ExternalDrivers/AD2S1210/tb",
+            "Components/ExternalDrivers/AD2S1210/tb",
+            "Components/system/axi_stream/combiner/rtl",
+            "Components/comms/SPI/rtl/master",
+            "Components/system/EnableGenerator/rtl",
+            "Components/comms/SPI/rtl",
+            "Components/system/axi_lite/crossbar/rtl/",
+            "Components/system/axi_lite/simple_register_cu/rtl",
+            "Components/system/axi_lite/skid_buffer/rtl"
+    };
+
+    auto prefix = "check_files/test_data/";
+    for(auto &p:paths){
+        for(auto &f:std::filesystem::directory_iterator(prefix + p)){
+            if(f.path().extension() == ".v" || f.path().extension() == ".sv" || f.path().extension() == ".svh"){
+                sv_analyzer analyzer(f.path());
+                analyzer.cleanup_content("`(.*)");
+
+                for(auto &entity:analyzer.analyze()){
+                    d_store->store_hdl_entity(entity);
+                }
+            }
+        }
+    }
+
+    s_store->set_setting("hdl_store", "/tmp/rb");
+
+    HDL_ast_builder_v2 b(s_store, d_store, Depfile());
+    auto ast_v2 = b.build_ast(std::vector<std::string>({"ad2s1210_tl"}))[0];
+
+
+    Dependency_resolver_v2 sim_r({ast_v2}, d_store);
+    auto sources = sim_r.get_dependencies();
+
+    xilinx_project_generator generator(s_store);
+
+    project_data d;
+    d.name = "test_proj";
+    d.synth_sources = sources;
+    d.sim_sources = {};
+    d.synth_tl = "ad2s1210_tl";
+    d.commons_dir = {};
+    d.repo_dir = "/tmp/tb";
+    generator.set_data(d);
+
+
+    std::ostringstream result_tcl;
+    generator.generate_synth_script(result_tcl);
+
+    const std::string expected_tcl = "set outputDir ./project_output\nfile mkdir $outputDir\nset sources_set {\n\tcheck_files/test_data/Components/Common/interfaces.sv\n\tcheck_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/AD2S1210.sv\n\tcheck_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/ad2s1210_CU.sv\n\tcheck_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/ad2s1210_configurator.sv\n\tcheck_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/ad2s1210_fault_handler.sv\n\tcheck_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/ad2s1210_reader.sv\n\tcheck_files/test_data/Components/ExternalDrivers/AD2S1210/rtl/ad2s1210_tl.sv\n\tcheck_files/test_data/Components/comms/SPI/rtl/Spi.sv\n\tcheck_files/test_data/Components/comms/SPI/rtl/master/ClockGen.sv\n\tcheck_files/test_data/Components/comms/SPI/rtl/master/SpiControlUnit.sv\n\tcheck_files/test_data/Components/comms/SPI/rtl/master/SpiRegister.sv\n\tcheck_files/test_data/Components/comms/SPI/rtl/master/Spi_master.sv\n\tcheck_files/test_data/Components/comms/SPI/rtl/master/TransferEngine.sv\n\tcheck_files/test_data/Components/system/EnableGenerator/rtl/Enable_Generator_2.sv\n\tcheck_files/test_data/Components/system/EnableGenerator/rtl/enable_comparator.v\n\tcheck_files/test_data/Components/system/EnableGenerator/rtl/enable_generator_core.v\n\tcheck_files/test_data/Components/system/EnableGenerator/rtl/enable_generator_counter.v\n\tcheck_files/test_data/Components/system/axi_lite/crossbar/rtl/address_decoder.sv\n\tcheck_files/test_data/Components/system/axi_lite/crossbar/rtl/axil_crossbar.sv\n\tcheck_files/test_data/Components/system/axi_lite/crossbar/rtl/axil_crossbar_wrapper.sv\n\tcheck_files/test_data/Components/system/axi_lite/simple_register_cu/rtl/axil_simple_register_cu.sv\n\tcheck_files/test_data/Components/system/axi_lite/skid_buffer/rtl/axil_skid_buffer.sv\n\tcheck_files/test_data/Components/system/axi_stream/combiner/rtl/axi_stream_combiner_3.sv\n}\nset inc_dirs {\n}\nset constr_dirs {\n}\nread_verilog $sources_set\nread_xdc $constr_dirs\nsynth_design -top ad2s1210_tl -part xc7s25ftgb196-1 -include_dirs $inc_dirs\nwrite_checkpoint -force $outputDir/post_synth.dcp\nopt_design\nplace_design\nwrite_checkpoint  -force $outputDir/post_place.dcp\nroute_design\nwrite_checkpoint  -force $outputDir/post_route.dcp\nwrite_bitstream -force $outputDir/top_module.bit\n";
+
     EXPECT_EQ(expected_tcl, result_tcl.str());
 }
